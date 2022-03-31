7cff1d1469f84b939e207c781712f789a0f14e58
Sabrina <36714185+skorsch@users.noreply.github.com>
2022-03-31 12:58:56 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -1,20 +1,83 @@
 terraform {
   required_providers {
-    aws =  {
-    source = "hashicorp/aws"
-    version = ">= 2.7.0"
+    google =  {
+    source = "hashicorp/google"
+    version = ">= 4.10.0"
     }
   }
 }
 
-provider "aws" {
-    region = "us-west-2"
+provider "google" {
+    project = "devxp-339721"
+    region = "us-west1"
 }
 
-resource "aws_s3_bucket" "terraform_backend_bucket" {
-      bucket = "terraform-state-yt3st9keh78w56yamnhak3wcxme7gibck86viu1mslxrl"
+resource "google_storage_bucket" "terraform_backend_bucket" {
+      location = "us-west1"
+      name = "terraform-state-5tkjwht6v82xph43tmmwxqgpj7drcw8aucs13c3lpwej4"
+      project = "devxp-339721"
+}
+
+resource "google_cloud_run_service" "cloud-run-gbbt" {
+      name = "cloud-run-gbbt"
+      location = "us-west1"
+      autogenerate_revision_name = true
+      template {
+        spec {
+          containers {
+            image = "gcr.io/devxp-339721/devxp:b79f5c8"
+            env {
+              name = "CONNECTION_STRING"
+              value = var.CLOUD_RUN_CONNECTION_STRING
+            }
+            env {
+              name = "GITHUB_CLIENT_ID"
+              value = var.CLOUD_RUN_GITHUB_CLIENT_ID
+            }
+            env {
+              name = "GITHUB_CLIENT_SECRET"
+              value = var.CLOUD_RUN_GITHUB_CLIENT_SECRET
+            }
+          }
+        }
+      }
+      traffic {
+        percent = 100
+        latest_revision = true
+      }
+      depends_on = [google_project_service.cloud-run-gbbt-service]
+}
+
+resource "google_cloud_run_service_iam_member" "cloud-run-gbbt-iam" {
+      service = google_cloud_run_service.cloud-run-gbbt.name
+      location = google_cloud_run_service.cloud-run-gbbt.location
+      project = google_cloud_run_service.cloud-run-gbbt.project
+      role = "roles/run.invoker"
+      member = "allUsers"
+}
+
+resource "google_project_service" "cloud-run-gbbt-service" {
+      disable_on_destroy = false
+      service = "run.googleapis.com"
 }
 
 
+variable "CLOUD_RUN_CONNECTION_STRING" {
+    type = string
+    sensitive = true
+}
 
+variable "CLOUD_RUN_GITHUB_CLIENT_ID" {
+    type = string
+    sensitive = true
+}
+
+variable "CLOUD_RUN_GITHUB_CLIENT_SECRET" {
+    type = string
+    sensitive = true
+}
+
+output "cloud-run-gbbt-service-url" {
+    value = google_cloud_run_service.cloud-run-gbbt.status[0].url
+}
 
