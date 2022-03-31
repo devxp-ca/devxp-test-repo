6260107fdec7c64b383dff70225ccaa23593d23a
Brennan Wilkes <brennan@codexwilkes.com>
2022-03-31 00:57:38 -0700

    DevXP: Initialized Terraform

--- terraform.tf
+++ terraform.tf
@@ -18,32 +18,6 @@ resource "google_storage_bucket" "terraform_backend_bucket" {
       project = "devxp-339721"
 }
 
-resource "google_storage_bucket" "storage-bucket-ydzy-lssl-iody-ngoj-noor" {
-      name = "storage-bucket-ydzy-lssl-iody-ngoj-noor"
-      location = "us-west1"
-      project = "devxp-339721"
-}
-
-resource "google_compute_instance" "gce-pspr" {
-      name = "gce-pspr"
-      machine_type = "f1-micro"
-      zone = "us-west1-a"
-      network_interface {
-        network = "default"
-      }
-      boot_disk {
-        initialize_params {
-          image = "ubuntu-2004-focal-v20220204"
-        }
-      }
-      project = "devxp-339721"
-}
-
-resource "google_project_service" "gce-pspr-service" {
-      disable_on_destroy = false
-      service = "compute.googleapis.com"
-}
-
 resource "google_cloud_run_service" "test-run-devxp" {
       name = "test-run-devxp"
       location = "us-west1"
@@ -101,5 +75,9 @@ variable "CLOUD_RUN_GITHUB_CLIENT_ID" {
 variable "CLOUD_RUN_GITHUB_CLIENT_SECRET" {
     type = string
     sensitive = true
+}
+
+output "test-run-devxp-service-url" {
+    value = google_cloud_run_service.test-run-devxp.status[0].url
 }
 
